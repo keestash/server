a263afbdb64b53cd0f35b38f99dcd3735188544d
Dogan Ucar <dogan@dogan-ucar.de>
2019-10-14 21:26:54 +0200

    add core app

---
+++ apps/apps/js/src/apps.js
@@ -0,0 +1,40 @@
+import Formula from "../../../../lib/js/src/Formula";
+import routes from "../../../../lib/js/src/Backend/routes";
+
+(function () {
+    if (!Keestash.AppsApp) {
+        Keestash.AppsApp = {};
+    }
+
+    Keestash.AppsApp = {
+
+        listen: function () {
+            $(".apps__app__checkbox").on("click", function () {
+                const checked = $(this).prop("checked");
+                const appId = $(this).attr("data-app-id");
+                const formula = new Formula();
+
+                formula.post(
+                    routes.getAppsUpdate(appId)
+                    , {
+                        "activate": true === checked
+                        , "app_id": appId
+                    },
+                    function (x) {
+                        console.log(x)
+                    }
+                );
+
+            });
+        }
+
+    }
+
+})
+();
+
+$(document).ready(function () {
+    console.log("test");
+    Keestash.AppsApp.listen();
+});
+
---
+++ apps/apps/lib/Api/UpdateApp.php
@@ -0,0 +1,118 @@
+<?php
+declare(strict_types=1);
+/**
+ * Keestash
+ *
+ * Copyright (C) <2019> <Dogan Ucar>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace KSA\Apps\Api;
+
+use Keestash\Api\AbstractApi;
+use Keestash\Core\Permission\PermissionFactory;
+use KSP\Api\IResponse;
+use KSP\App\Config\IApp;
+use KSP\Core\Repository\AppRepository\IAppRepository;
+use KSP\L10N\IL10N;
+
+class UpdateApp extends AbstractApi {
+
+    private $parameters    = null;
+    private $appRepository = null;
+
+    public function __construct(
+        IL10N $l10n
+        , IAppRepository $appRepository
+    ) {
+        parent::__construct($l10n, true);
+
+        $this->appRepository = $appRepository;
+    }
+
+    public function onCreate(...$params): void {
+        $this->parameters = $params[0];
+
+        parent::setPermission(
+            PermissionFactory::getDefaultPermission()
+        );
+    }
+
+    public function create(): void {
+        $appId    = $this->parameters["app_id"] ?? null;
+        $activate = $this->parameters["activate"] ?? null;
+
+
+        if (null === $activate || false === is_bool((bool) $activate)) {
+            $response = parent::createResponse(
+                IResponse::RESPONSE_CODE_NOT_OK
+                , [
+                    "message" => "No Action Defined"
+                ]
+            );
+
+            parent::setResponse($response);
+            return;
+        }
+
+        if (false === is_string($appId)) {
+            $response = parent::createResponse(
+                IResponse::RESPONSE_CODE_NOT_OK
+                , [
+                    "message" => "No App Id Given"
+                ]
+            );
+
+            parent::setResponse($response);
+            return;
+        }
+
+        /** @var IApp $app */
+        $app = $this->appRepository->getApp($appId);
+
+        if (null === $app) {
+            $response = parent::createResponse(
+                IResponse::RESPONSE_CODE_NOT_OK
+                , [
+                    "message" => "No App Found"
+                ]
+            );
+
+            parent::setResponse($response);
+            return;
+        }
+
+        $app->setEnabled($activate === IApp::ENABLED_TRUE);
+
+        $replaced = $this->appRepository->replace($app);
+
+        $response = parent::createResponse(
+            $replaced ? IResponse::RESPONSE_CODE_OK : IResponse::RESPONSE_CODE_NOT_OK
+            , [
+                "message" => $replaced ? "App updated" : "No App Found"
+            ]
+        );
+
+        parent::setResponse($response);
+        return;
+
+
+    }
+
+    public function afterCreate(): void {
+
+    }
+
+}
