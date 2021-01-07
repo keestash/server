77c66cea7991f5ff43106350797e13e088286786
Dogan Ucar <dogan@dogan-ucar.de>
2021-01-07 23:09:31 +0100

    remove unused checks

--- bin/scss.php
+++ bin/scss.php
@@ -28,9 +28,6 @@ use ScssPhp\ScssPhp\Compiler as ScssCompiler;
 
   chdir(dirname(__DIR__));
 
-  require_once __DIR__ . '/../lib/versioncheck.php';
-  require_once __DIR__ . '/../lib/filecheck.php';
-  require_once __DIR__ . '/../lib/extensioncheck.php';
   require_once __DIR__ . '/../vendor/autoload.php';
 
   compile(
