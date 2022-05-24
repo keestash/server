9593afda14e8f48cfb59295e43a70ea197d04915
Doğan Uçar <dogan@dogan-ucar.de>
2022-05-24 14:24:30 +0200

    * fix return type error

--- lib/private/Core/DTO/File/File.php
+++ lib/private/Core/DTO/File/File.php
@@ -81,11 +81,11 @@ class File implements IFile {
      * Specify data which should be serialized to JSON
      *
      * @link  https://php.net/manual/en/jsonserializable.jsonserialize.php
-     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * @return array data which can be serialized by <b>json_encode</b>,
      * which is a value of any type other than a resource.
      * @since 5.4.0
      */
-    public function jsonSerialize() {
+    public function jsonSerialize(): array {
         return [
             "id"          => $this->getId()
             , "name"      => $this->getName()
